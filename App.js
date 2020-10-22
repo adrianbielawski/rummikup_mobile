48e75361816dfe5f68f14f5ce2799f90df58b2ba
Adrian Bielawski <adrian@bielaw.ski>
2020-10-22 10:44:38 +0100

    Fonts added

--- App.js
+++ App.js
@@ -1,9 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { AppLoading } from 'expo';
+import { useFonts } from '@use-expo/font';
 import { StyleSheet } from 'react-native';
 import Main from './src/main';
 function App() {
+    let [fontsLoaded] = useFonts({
+        'nunito-regular': require('./assets/fonts/Nunito-Regular.ttf'),
+        'nunito-bold': require('./assets/fonts/Nunito-Bold.ttf'),
+        'nunito-extraBold': require('./assets/fonts/Nunito-ExtraBold.ttf'),
+    });
 
     return (
         <Provider store={store}>
